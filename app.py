7b302b1a58544631136d0fc661ac6d6b9ca58dcc
Paul Schofield <paul.schofield.us@gmail.com>
2023-03-13 09:11:49 -0400

    refactor with pages, login and css

--- app.py
+++ app.py
@@ -4,7 +4,26 @@ import psycopg2
 from datetime import datetime
 import json
 
-app = Flask(__name__)
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
 
 @app.route("/", methods=['GET', 'POST'])
 def log_request():
@@ -34,72 +53,3 @@ def log_request():
     conn.close()
 
     return render_template('sent.html')
-
-
-@app.route('/message/', methods=['GET', 'POST'])
-def new_message():
-        # Connect to the database 
-    conn = psycopg2.connect(
-        host='localhost',
-        port='5432',
-        dbname='paulschofield-personal',
-        user='paulschofield-personal',
-        password='root'
-    )
-    cur = conn.cursor()
-
-        # Insert the request data into the database
-    headers = json.dumps(dict(request.headers))
-    body = json.dumps(dict(request.form))
-    cur.execute("INSERT INTO requests (method, path, headers, body, created_at) VALUES (%s, %s, %s, %s, %s)", (
-        request.method,
-        request.path,
-        headers,
-        body,
-        datetime.now()
-    ))
-    conn.commit()
-
-    # Close the connection
-    cur.close()
-    conn.close()
-
-    ##input_message = input('Enter message')
-    return render_template('sent.html')
-
-@app.route('/retrieve/', methods=['GET'])
-def get_message():
-            # Connect to the database 
-    conn = psycopg2.connect(
-        host='localhost',
-        port='5432',
-        dbname='paulschofield-personal',
-        user='paulschofield-personal',
-        password='root'
-    )
-    cur = conn.cursor()
-
-            # Insert the request data into the database
-    headers = json.dumps(dict(request.headers))
-    body = json.dumps(dict(request.form))
-    cur.execute("INSERT INTO requests (method, path, headers, body, created_at) VALUES (%s, %s, %s, %s, %s)", (
-        request.method,
-        request.path,
-        headers,
-        body,
-        datetime.now()
-    ))
-    conn.commit()
-
-        #Query recent messages; option to specify number of messages
-    #Substituting for user input == number of messages to retriev
-    user_input = 3
-    cur.execute("SELECT body FROM requests WHERE method='POST' ORDER BY created_at DESC LIMIT {:d}".format(user_input))
-    for record in cur:
-        print(record)
-
-
-    cur.close()
-    conn.close()
-
-    return render_template('send.html')
